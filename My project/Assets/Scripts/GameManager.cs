dd79718523ab201446426a6182281c2dc821379a
VolA1GhT <dundurs1.4@gmail.com>
2026-02-09 15:48:19 +0200

    ZombieProject

---
+++ My project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] zombies;
+    public GameObject selectedZombie;
+    void Start()
+    {
+        selectedZombie = zombies[2];
+    }
+
+    void Update()
+    {
+        
+    }
+}
